0a24ce141b19bb6b1de9e1b0346a676efbfa5f86
Farron <jones.f03@mymail.sxu.edu>
2023-05-09 01:30:26 -0500

    Update Main.java
    Updated how the dice game is played.

--- Main.java
+++ Main.java
@@ -26,7 +26,7 @@ public class Main {
 							System.out.println("Go find a different game, seriously.");
 							//breaks user from seeing the game code
 							break;
-						}
+						}//end if
 						//if canplay is equal to true, prompt user to play the game.
 						if(canplay==true) {
 							System.out.println("Let's begin the game!");
@@ -39,19 +39,21 @@ public class Main {
 							switch(decision) {
 							case 1: System.out.println("You are now gambling with dice:");
 							//game method 1 go here
+							//can call Dicegame method multiple times
 							Dicegame();
+							//break
 							break;
 							case 2: System.out.println("GTA is now playing.");
 							//game method 2 go here
 							//break;
 							break;
-							}
+							}//end switch
 							
-						}
-					}
+						}//end if
+					}//end if
 				//while x is less than 1 and canplay is equal to false
 				}while(x<1&&canplay==false);
-			}
+			}//end main
 			//hello method
 			public static void Hello() {
 				//declare scanner
@@ -79,15 +81,15 @@ public class Main {
 					System.out.println("You can play the game!");
 					//return true
 					return true;
-				}
+				}//end if
 				//else statement
 				else {
 					//cannot play game
 					System.out.println("Game is for mature audiences only!");
 					//return false;
 					return false;
-				}
-			}
+				}//end else
+			}//end checkifplaygame method
 			//Dicegame method
 			public static void Dicegame() {
 				//int roll as 0
@@ -100,33 +102,47 @@ public class Main {
 				Random random = new Random();
 				//prompt user rolls
 				System.out.println("Your rolls is as followed:");
+				//your bankaccount is between 1 and a hundred
+				Double bankaccount =random.nextDouble(100)+1;
+				System.out.println("Your bankaccount contains:"+df.format(bankaccount));
 				//does the code
 				do {
-				//dicegame is rolled 1-6
-				int dicegame = random.nextInt(6)+1;
+				//dicegame1 is rolled 1-6
+				int dicegame1 = random.nextInt(6)+1;
+				//dicegame2 is rolled 1-6
+				int dicegame2 = random.nextInt(6)+1;
 				//rollnum is being incremented
 				rollnum++;
 				//prints out the dicegame rolls
-				System.out.println(dicegame);
+				System.out.println(dicegame1);
+				System.out.println(dicegame2);
 				//if at least two of the dice rolled the same value, you don't have to pay
-				//if(dicegame==1&&dicegame==1||dicegame==2&&dicegame==2||dicegame==3&&dicegame==3||dicegame==4&&dicegame==4||dicegame==5&&dicegame==5||dicegame==6&&dicegame==6) {
-					if(dicegame==dicegame) {
+					if(dicegame1==dicegame2) {
 					System.out.println("You do not have to pay!");
-					//roll=false, to not roll again
-					//roll=true;
-				}
-				//else if its different values, you have to pay
-				else if(dicegame!=dicegame) {
-					System.out.println("You have to pay!");
+					System.out.println("You won the game!");
+					System.out.println("You keep the earnings of: "+df.format(bankaccount));
 					//roll is equal to false, to not roll again
 					roll=false;
-					}
-				//rollnum is less than 6 and roll is equal to true
+				}//end if
+				//else if its different values, you have to pay
+				else if(dicegame1!=dicegame2) {
+					System.out.println("You have to pay!");
+					//roll is equal to true, to roll again
+					roll=true;
+					//if you loose all your money before you do not have to pay or if rollnum reaches 6 times, you loose the game
+					if(rollnum==6||bankaccount==0) {
+						System.out.println("You lost the game!");
+						System.out.println("Your earnings is: "+df.format(bankaccount)+" and you on roll number: "+rollnum);
+						//roll is equal to false, to not roll again
+						roll=false;
+					}//end if
+					}//end else if
+				//while rollnum is less than 6 and roll is equal to true, keep rolling till you do not have to pay
 				}while(rollnum<6&&roll==true);
-				//your bankaccount is between 1 and a hundred
-				//find out how your bankaccount will change when you roll the dice for this method
-				Double bankaccount =random.nextDouble(100)+1;
-				System.out.println("Your bankaccount contains:"+df.format(bankaccount));
-			}
+				//THING TO FOCUS ON:
+				//Find out how your bankaccount will change when you roll the dice for this method
+				
+			}//end Dicegame method
 
-	}
+	}//end class
+
