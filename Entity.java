4d673abd40046fc304a2d413887dfa8b30020799
Farron <jones.f03@mymail.sxu.edu>
2023-07-09 03:26:54 -0500

    Create Entity.java
    Added the Entity class which is meant for storing variables for our game. Like for players, monsters, and NPC classes.

---
+++ Entity.java
@@ -0,0 +1,18 @@
+//package
+package entity;
+//imports
+import java.awt.image.BufferedImage;
+
+//public class Entity
+//Stores variables that will be used in player,monster and NPC classes.
+public class Entity {
+	//public int x,y declared
+	public int x,y;
+	//public int speed declared
+	public int speed;
+	//To store image file
+	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+	//Declare String direction
+	public String direction;
+}//end class
+
