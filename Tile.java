e6d27a52288937fa226fd9898872840b710fcd40
Farron <jones.f03@mymail.sxu.edu>
2023-07-14 18:54:54 -0500

    Create Tile.java
    Created a Tile class with variables to help with Tiles displayed in our game.

---
+++ Tile.java
@@ -0,0 +1,12 @@
+//Package
+package tile;
+//imports
+import java.awt.image.BufferedImage;
+
+//public class Tile
+public class Tile {
+	//To store image
+	public BufferedImage image;
+	//The collision will be set to false
+	public boolean collision = false;
+}//end class
