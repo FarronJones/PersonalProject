79c3d1e57675aaeff450930b15566d63f725046e
Farron <jones.f03@mymail.sxu.edu>
2023-07-26 02:15:28 -0500

    Create SuperObject.java
    Basic object class for objects that will be in our game.

---
+++ SuperObject.java
@@ -0,0 +1,12 @@
+//package object
+package object;
+//imports
+import java.awt.image.BufferedImage;
+
+//public class SuperObject
+public class SuperObject {
+	//Declare stuff needed for objects
+	public BufferedImage image, image2, image3;
+	public String name;
+	public boolean collision = false;
+}//end class
