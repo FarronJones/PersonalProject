1145796effa1ac86cc93806b50bf2598cafc5d9d
Farron <jones.f03@mymail.sxu.edu>
2023-07-26 02:17:00 -0500

    Create OBJ_Heart.java
    Making the heart images for the player to see the health.

---
+++ OBJ_Heart.java
@@ -0,0 +1,29 @@
+//package object
+package object;
+//imports
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import Main.GamePanel;
+//public class OBJ_Heart
+public class OBJ_Heart extends SuperObject {
+	//GamePanel gp
+	GamePanel gp;
+	//public OBJ_Heart
+	public OBJ_Heart(GamePanel gp) {
+		//this.gp equal to gp
+		this.gp=gp;
+		//name equal to Heart
+		name = "Heart";
+		//try
+		try {
+			//set health images
+			image = ImageIO.read(getClass().getResourceAsStream("/objects/heart_full.png"));
+			image2 = ImageIO.read(getClass().getResourceAsStream("/objects/heart_half.png"));
+			image3 = ImageIO.read(getClass().getResourceAsStream("/objects/heart_blank.png"));
+		}//end try
+		//catch
+		catch(IOException e) {
+			e.printStackTrace();
+		}//end catch
+}//end OBJ_Heart method
+}//end class
