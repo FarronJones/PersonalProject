d6b860182b1b4026fccbb0b4a34223ad92c18076
Farron <jones.f03@mymail.sxu.edu>
2023-07-30 21:36:08 -0500

    Update SuperObject.java
    Added uTool.

--- SuperObject.java
+++ SuperObject.java
@@ -2,11 +2,13 @@
 package object;
 //imports
 import java.awt.image.BufferedImage;
-
+import Main.UtilityTool;
 //public class SuperObject
 public class SuperObject {
 	//Declare stuff needed for objects
 	public BufferedImage image, image2, image3;
 	public String name;
 	public boolean collision = false;
+	UtilityTool uTool = new UtilityTool();
+
 }//end class
