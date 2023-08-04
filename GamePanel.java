3b5f45a5b88deae4c984d0ca768bb6c55d3e230a
Farron <jones.f03@mymail.sxu.edu>
2023-08-03 22:52:26 -0500

    Update GamePanel.java
    Added font and heart objects to the game panel to be able to use them. Added game state to know when the game is running to execute specific methods like "drawPlayerLife." There is now its own draw method in the GamePanel class to help draw hearts with the support of the "drawPlayerLife" method.

--- GamePanel.java
+++ GamePanel.java
@@ -3,10 +3,18 @@ package Main;
 //imports
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.swing.JPanel;
 import entity.Player;
+import object.OBJ_Heart;
+import object.SuperObject;
 import tile.TileManager;
 
 //public class extends JPanel class which have functions
@@ -28,6 +36,10 @@ public class GamePanel extends JPanel implements Runnable {
 	//declare Thread gameThread
 	Thread gameThread;
 	
+	//Game State
+	public int gameState;
+	public final int playState=1;
+	
 	//FPS
 	int FPS = 60;
 	
@@ -36,20 +48,57 @@ public class GamePanel extends JPanel implements Runnable {
 	//instantiate TileManager
 	TileManager tileM = new TileManager(this);
 	
-	//constructor
-	public GamePanel() {
-		//set size of GamePanel
-		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-		//set background color
-		this.setBackground(Color.black);
-		//improve game rendering performance
-		this.setDoubleBuffered(true);
-		//add key input to panel
-		this.addKeyListener(keyH);
-		//GamePanel can be focused to receive key input
-		this.setFocusable(true);
-	}//end constructor
+	//For heart image
+	Graphics2D g2;
+	BufferedImage heart_full,heart_half, heart_blank;
 	
+	//For font
+	Font maruMonica;
+	//GamePanel
+	GamePanel gp;
+
+	
+	//constructor
+	public GamePanel(GamePanel gp) {
+		//this.gp=gp;
+		this.gp=gp;
+		//try
+		try {
+			//set size of GamePanel
+			this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+			//set background color
+			this.setBackground(Color.black);
+			//improve game rendering performance
+			this.setDoubleBuffered(true);
+			//add key input to panel
+			this.addKeyListener(keyH);
+			//GamePanel can be focused to receive key input
+			this.setFocusable(true);
+			//inputstream is equal get the font
+			InputStream is = getClass().getResourceAsStream("/font/x12y16pxMaruMonica.ttf");
+			//maruMonica equal Fonnt is created
+			maruMonica = Font.createFont(Font.TRUETYPE_FONT,is);
+		}//end try
+		//catch
+		catch(FontFormatException e) {
+			e.printStackTrace();
+		}//end catch
+		//catch
+		catch(IOException e) {
+			e.printStackTrace();
+		}//end catch
+		//create HUD OBJECT
+		SuperObject heart = new OBJ_Heart(gp);
+		//set heart to images
+		heart_full=heart.image;
+		heart_half=heart.image2;
+		heart_blank=heart.image3;
+	}//end constructor
+	//public void setupGame
+	public void setupGame() {
+		//gameState equal to playState
+		gameState=playState;
+	}//end setupGame
 	//startGameThread
 	public void startGameThread() {
 		gameThread = new Thread(this);
@@ -97,8 +146,11 @@ public class GamePanel extends JPanel implements Runnable {
 	}//end run method
 	//update
 	public void update() {
+		//if gameState equal to playState
+		if(gameState==playState) {
 		//calls player update method
 		player.update();
+		}//end if
 	}//end update method
 	//paintComponent1 method
 	public void paintComponent(Graphics g) {
@@ -110,7 +162,41 @@ public class GamePanel extends JPanel implements Runnable {
 		tileM.draw(g2);
 		//call player draw method
 		player.draw(g2);
+		//call draw method for hearts
+		draw(g2);
 		//good practice to save memory
 		g2.dispose();
 	}//end paintComponent method
+	//public void draw
+	public void draw(Graphics2D g2) {
+		//this.g2 equal to g2
+		this.g2=g2;
+		//set font to maruMonica
+		g2.setFont(maruMonica);
+		//set rendering
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		//set color to white
+		g2.setColor(Color.white);
+		//if gp.gameState equal equal to gp.playState
+		if(gp.gameState==gp.playState) {
+		//drawPlayerLife method
+		drawPlayerLife();
+		}//end if
+	}//end draw method
+	//public void drawPlayerLife method
+	public void drawPlayerLife() {
+		//int x and y is tileSize divided by 2
+		int x = gp.tileSize/2;
+		int y = gp.tileSize/2;
+		//int i equal to zero
+		int i = 0;
+		//while i is less than player.maxLife/2
+		while(i<player.maxLife/2) {
+			g2.drawImage(heart_blank,x,y,null);
+			//i is being incremented
+			i++;
+			//increases x by tileSize
+			x+=gp.tileSize;
+		}//end while loop
+	}//end drawPlayerLife method
 }//end class
