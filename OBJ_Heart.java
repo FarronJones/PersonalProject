d5f8958336dadbdf13f18bf8aff02fd73ea30e65
Farron <jones.f03@mymail.sxu.edu>
2023-08-05 22:07:39 -0500

    Update OBJ_Heart.java
    Syntax fix.

--- OBJ_Heart.java
+++ OBJ_Heart.java
@@ -21,8 +21,8 @@ public class OBJ_Heart extends SuperObject {
 			image2 = ImageIO.read(getClass().getResourceAsStream("/objects/heart_half.png"));
 			image3 = ImageIO.read(getClass().getResourceAsStream("/objects/heart_blank.png"));
 			image  = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
-			image2 = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
-			image3 = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+			image2 = uTool.scaleImage(image2, gp.tileSize, gp.tileSize);
+			image3 = uTool.scaleImage(image3, gp.tileSize, gp.tileSize);
 		}//end try
 		//catch
 		catch(IOException e) {
