72b13d564187f59ab2dec2fc855b7f2455231174
Farron <jones.f03@mymail.sxu.edu>
2023-12-27 01:09:10 -0600

    Update Entity.java
    Implement a shared setup method to help with all new NPCs images.

--- Entity.java
+++ Entity.java
@@ -2,10 +2,16 @@
 package entity;
 //imports
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import Main.GamePanel;
+import Main.UtilityTool;
 
 //public class Entity
 //Stores variables that will be used in player,monster and NPC classes.
 public class Entity {
+	//GamePanel gp
+	GamePanel gp;
 	//public int x,y declared
 	public int x,y;
 	//public int speed declared
@@ -20,8 +26,31 @@ public class Entity {
 	public int spriteNum = 1;
 	//public int actionLockCounter equal to zero
 	public int actionLockCounter=0;
-
 	//Character Status
 	public int maxLife;
 	public int life;
+	//public Entity 
+	public Entity(GamePanel gp) {
+		//this.gp=gp
+		this.gp=gp;
+	}//end public entity
+	//BufferedImage setup method
+			public BufferedImage setup(String imagePath) {
+				//instantiate utilityTool
+				UtilityTool uTool = new UtilityTool();
+				//image is equal to null
+				BufferedImage image = null;
+						//try
+						try {
+							//scale the player images
+							image = ImageIO.read(getClass().getResourceAsStream(imageName+".png"));
+							image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+						}//end try
+						//catch
+						catch(IOException e) {
+							e.printStackTrace();
+						}//end catch
+						//return image
+						return image;
+			}//end BufferedImage setup method
 }//end class
