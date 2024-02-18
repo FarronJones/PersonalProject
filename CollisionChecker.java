d4225e2c01b33f1c4baf10a2e971860c3ee17f46
Farron <jones.f03@mymail.sxu.edu>
2024-02-17 23:08:53 -0600

    Create CollisionChecker.java
    CollisionChecker class created for objects to detect Collision.

---
+++ CollisionChecker.java
@@ -0,0 +1,19 @@
+//package
+package Main;
+//imports
+import entity.Entity;
+
+//public class CollisionChecker
+public class CollisionChecker {
+	//Gamepanel gp
+	GamePanel gp;
+	//public CollisionChecker
+	public CollisionChecker(GamePanel gp) {
+		//this.gp=gp
+		this.gp=gp;
+	}//end public CollisionChecker
+	//public void checkTile
+	public void checkTile(Entity entity) {
+		
+	}//checkTile
+}//end public class CollisionChecker
