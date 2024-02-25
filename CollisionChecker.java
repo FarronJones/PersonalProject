b2dc16e57a2368766e28a3ae7f9bb4c5783f6cad
Farron <jones.f03@mymail.sxu.edu>
2024-02-25 00:36:21 -0600

    Update CollisionChecker.java
    Method for player and monster collision.

--- CollisionChecker.java
+++ CollisionChecker.java
@@ -70,4 +70,59 @@ public class CollisionChecker {
 			break;
 		}//end switch
 	}//checkTile
+	//NPC or Monster
+	public int checkEntity(Entity entity,Entity[]target) {
+		//int index=999
+		int index=999;
+		
+		//for loop to go through target.length
+		for(int i=0; i<target.length;i++) {
+			if(target[i]!=null) {
+				//Get entity's solid area position
+				entity.solidArea.x=entity.worldX+entity.solidArea.x;
+				entity.solidArea.y=entity.worldY+entity.solidArea.y;
+				
+				//Get the targets's solid area position
+				target[i].solidArea.x=target[i].worldX+target[i].solidArea.x;
+				target[i].solidArea.y=target[i].worldY+target[i].solidArea.y;
+				
+				//switch 
+				switch(entity.direction) { 
+				case "up":
+					entity.solidArea.y-=entity.speed;
+					if(entity.solidArea.intersects(target[i].solidArea)) {
+							entity.collisionOn=true;
+							index=i;
+					}///end if
+					break;
+				case "down":
+					entity.solidArea.y+=entity.speed;
+					if(entity.solidArea.intersects(target[i].solidArea)) {
+						entity.collisionOn=true;
+						index=i;
+					}///end if
+					break;
+				case "left":
+					entity.solidArea.x-=entity.speed;
+					if(entity.solidArea.intersects(target[i].solidArea)) {
+							entity.collisionOn=true;
+							index=i;
+					}///end if
+					break;
+				case "right":
+					entity.solidArea.x+=entity.speed;
+					if(entity.solidArea.intersects(target[i].solidArea)) {
+							entity.collisionOn=true;
+							index=i;
+					}///end if
+					break;
+				}//end switch
+			}//end if
+			entity.solidArea.x=entity.solidAreaDefaultX;
+			entity.solidArea.y=entity.solidAreaDefaultY;
+			target[i].solidArea.x=target[i].solidAreaDefaultX;
+			target[i].solidArea.y=target[i].solidAreaDefaultY;
+		}//end for loop
+		return index;
+	}//end checkEntity method
 }//end public class CollisionChecker
